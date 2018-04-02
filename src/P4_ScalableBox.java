be1b6e245097cae92e15e30defab067fa3ae1db4
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 23:57:21 -0500

    Update P4_ScalableBox.java

--- src/P4_ScalableBox.java
+++ src/P4_ScalableBox.java
@@ -1,41 +1,57 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Clayton Bruce
+	Date: 4/1/2018
 */
+
+import java.util.Scanner;
 
 public class P4_ScalableBox {
 
 	public static void main(String[] args) {
-		drawBoxCap();
-		drawBoxInsides();
-		drawBoxCap();
+		
+		Scanner console = new Scanner(System.in); 
+		System.out.print("Input Box width? :  "); 
+		int width = console.nextInt();
+		
+		
+		System.out.print("Input Box Height? :  "); 
+		int height = console.nextInt();
+		
+		drawBoxCap(width);
+		drawBoxInsides(height);
+		drawBoxCap(width);
 	}
 	
-	public static void drawBoxCap() {
-		System.out.print("+");
+	public static int drawBoxCap(int width) {
 		
-		for( int capDashIndex = 1; capDashIndex <= 8 - 2; capDashIndex++ ) {
+		//width below also
+		System.out.print("+");
+		for( int capDashIndex = 1; capDashIndex <= width - 2; capDashIndex++ ) {
 			System.out.print("-");
 		}
 		
 		System.out.println("+");
+		return width;
 	}
 	
-	public static void drawBoxInsides() {
-		for( int insideRowIndex = 1; insideRowIndex <= 10 - 2; insideRowIndex++ ) {
-			drawBoxInsideLine();
+	public static void drawBoxInsides(int height) {
+		for( int insideRowIndex = 1; insideRowIndex <= height - 2; insideRowIndex++ ) {
+			drawBoxInsideLine(height);
 		}
 	}
 	
-	public static void drawBoxInsideLine() {
-		System.out.print("|");
+	public static int drawBoxInsideLine(int width) {
 		
-		for( int insideRowIndex = 1; insideRowIndex <= 8 - 2; insideRowIndex++ ) {
+		//Width below
+		System.out.print("|");
+		for( int insideRowIndex = 1; insideRowIndex <= width - 2; insideRowIndex++ ) {
 			System.out.print(".");
 		}
 		
 		System.out.println("|");
+		return width;
 	}
+	
 
 }
