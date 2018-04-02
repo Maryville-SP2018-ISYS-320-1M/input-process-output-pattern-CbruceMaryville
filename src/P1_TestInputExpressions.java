1d3759a4aadd1866f2cbf92d58a9d7e4774b6eb1
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 23:34:38 -0500

    Update P1_TestInputExpressions.java

--- src/P1_TestInputExpressions.java
+++ src/P1_TestInputExpressions.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
 
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. works 
+ 2. works
+ 3. dollar sign causes errors
+ 4. the input type is not string
  
- 5.
- 6.
- 7.
- 8.
+ 5. multiplication doesnt work
+ 6. thats a number with a char in it so doesnt work
+ 7. string doesnt work
+ 8. works
+ 
+ The numbers work up to ints and doubles. They code doesnt do anything with the input though.
   
  */
 
