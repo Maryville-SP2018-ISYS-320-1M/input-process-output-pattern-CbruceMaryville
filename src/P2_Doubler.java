7b87a6a6b49dd3ee199488011e8cae4b1cb11386
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 23:37:28 -0500

    Update P2_Doubler.java

--- src/P2_Doubler.java
+++ src/P2_Doubler.java
@@ -1,13 +1,22 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Clayton Bruce
+	Date: 4/1/2018
 */
+
+import java.util.Scanner;
 
 public class P2_Doubler {
 
 	public static void main(String[] args) {
 
+		
+		
+		Scanner console = new Scanner(System.in); 
+		System.out.println("What is your number? "); 
+		int number = console.nextInt();
+		
+		System.out.println("Your number doubled is: " + number*2);
 	}
 
 }
