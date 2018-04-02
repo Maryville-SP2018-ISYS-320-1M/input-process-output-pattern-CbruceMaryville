8c78e7cffe9ed9f892a685ce2c36895151f157b5
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 23:43:21 -0500

    Update P3_EchoChamber.java

--- src/P3_EchoChamber.java
+++ src/P3_EchoChamber.java
@@ -1,13 +1,27 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Clayton Bruce
+	Date: 4/1/2018
 */
+
+import java.util.Scanner;
 
 public class P3_EchoChamber {
 
 	public static void main(String[] args) {
 
+		
+		
+		Scanner console = new Scanner(System.in); 
+		System.out.print("What is your phrase? :  "); 
+		String input = console.nextLine();
+		System.out.print("How many times do you want it repeated? : ");
+		int num = console.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+			System.out.println(input);
+		}
+		
 	}
 
 }
